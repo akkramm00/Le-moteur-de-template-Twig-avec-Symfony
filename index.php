1a27a15fe28ac00e0e69757b75e9516178de1d30
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-24 09:49:59 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Le monteur de template Twig</title>
+  </head>
+  <body>
+<h1>Utilisation et Syntaxe de Twig</h1>
+
+    <p>
+      Twig peut générer du texte de n'importe hquel format textuel(HTML, XML, CSV, Latex, etc...). Dans ce ours, nous partirons sur des exemples basés surle HTML, qui représentent la plus grande utilisation de Twig.
+
+      Comme nous l'avons vu précédemment, la force d'un motuer de templates comme Twigse situe dans le fait de pouvoir insérerdes variables , des expressions et des conditions PHP dans le HTML.
+      NOtons la syntaxe particulière qui est nativement supportée par Twig afin de nous apporter cette puissance:<br><br>
+
+      * {{ ... }} : Permet l'affichage d'une expression <br>
+      * {% ... %} : Permet d'exécuter une action.<br>
+      * {# ... #} : Permet d'ajouter des commentaires.<br>
+    </p>
+  </body>
+</html>
